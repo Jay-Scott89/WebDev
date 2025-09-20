eaa8164982604bde4edbc5de3352767514b6edba
Jay-Scott89 <jamie.scott3495@outlook.com>
2025-09-20 10:11:24 +0100

    conditions and arrays

---
+++ JavaScript/02_foundation/conditions.js
@@ -0,0 +1,47 @@
+// Check if a number is greater than another number:
+let num1 = 5;
+let num2 = 8;
+
+if (num1 > num2) {
+  console.log("num1 is greater than num2");
+} else {
+  console.log("num1 is not greater than num2");
+}
+
+// Check if a string is equal to another string
+
+let firstName = "Jamie";
+let username = "Jamie";
+
+if (firstName == username) {
+  console.log("Make an unique username");
+}
+
+// check if a variable is a number or not:
+let score = 44;
+
+if (typeof score === "number") {
+  console.log("Score is a number");
+} else {
+  console.log("Score is not a number");
+}
+
+// Check if a boolean value is true or false:
+
+let isTeaReady = false;
+
+if (isTeaReady) {
+  console.log("Tea is Ready");
+} else {
+  console.log("Tea is not Ready");
+}
+
+// Check is an array is empty or not:
+
+let items = [];
+
+if (items.length === 0) {
+  console.log("Array is empty");
+} else {
+  console.log("Array is not empty");
+}
