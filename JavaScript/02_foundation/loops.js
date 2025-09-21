2ac1a3c2b9799ddae30fd68c9f072bc078a92807
Jay-Scott89 <jamie.scott3495@outlook.com>
2025-09-21 21:15:21 +0100

    halfway through the loop challenges

--- JavaScript/02_foundation/loops.js
+++ JavaScript/02_foundation/loops.js
@@ -8,28 +8,69 @@ while (i <= 5) {
   sum += i;
   i++;
 }
-console.log(sum);
+console.log(`1. ${sum}`);
 
 /* 
 2. Write a `while` loop that counts down from 5 to 1 and stores the numbers in an array named `countdown`.
 */
-
+let countdown = [];
+let j = 5;
+while (j > 0) {
+  countdown.push(j);
+  j--;
+}
+console.log(`2. ${countdown}`);
 /* 
 3. Write a `do while` loop that prompts a user to enter their favorite tea type until they enter `"stop"`. 
    Store each tea type in an array named `teaCollection`.
 */
 
+// Must be run in the browser console
+/* let teaCollection = [];
+let tea;
+do {
+  tea = prompt(`Enter your favourite tea (type "stop to finish)`);
+  if (tea !== "stop") {
+    teaCollection.push(tea);
+  }
+} while (tea !== "stop"); */
+
 /* 
 4. Write a `do while` loop that adds numbers from 1 to 3 and stores the result in a variable named `total`.
 */
 
+let total = 0;
+let k = 1;
+do {
+  total += k;
+  k++;
+} while (k <= 3);
+console.log(`4. ${total}`);
 /* 
 5. Write a `for` loop that multiplies each element in the array `[2, 4, 6]` by 2 and stores the results in a new array named `multipliedNumbers`.
 */
 
+let multipliedNumbers = [];
+let numbers = [2, 4, 6];
+for (let l = 0; l < numbers.length; l++) {
+  let takenNumber = numbers[l] * 2;
+  multipliedNumbers.push(takenNumber);
+
+  // multipliedNumbers.push(numbers[l] * 2); Alternate way to write the same line
+}
+console.log(`5. ${multipliedNumbers} `);
+
 /* 
 6. Write a `for` loop that lists all the cities in the array `["Paris", "New York", "Tokyo", "London"]` and stores each city in a new array named `cityList`.
 */
+
+let cities = ["Paris", "New York", "Tokyo", "London"];
+let cityList = [];
+for (let i = 0; i < cities.length; i++) {
+  const myCity = cities[i];
+  cityList.push(myCity);
+}
+console.log(`6. ${cityList}`);
 
 // Higher Level Challenges
 /* 
