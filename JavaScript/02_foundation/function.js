d06630a76594eef3504f87e5cb4188dba78120e2
Jay Scott <jamie.scott89@live.co.uk>
2025-09-23 13:41:58 +0100

    Halfway through the function challenges

---
+++ JavaScript/02_foundation/function.js
@@ -0,0 +1,44 @@
+/* 
+1. Write a function named `makeTea` that takes one parameter, `typeOfTea`, and returns a string like `"Making green tea"` when called with `"green tea"`. 
+Store the result in a variable named `teaOrder`.
+*/
+
+function makeTea(typeOfTea) {
+    let teaOrder = typeOfTea
+    return `Making ${teaOrder}`;
+}
+console.log(makeTea("green tea"))
+/* 
+2. Create a function named `orderTea` that takes one parameter, `teaType`. Inside this function, create another function named `confirmOrder` that returns a message like `"Order confirmed for chai"`. 
+Call `confirmOrder` from within `orderTea` and return the result.
+*/
+
+function orderTea(teaType) {
+    function confirmOrder(orderTea) {
+        return `Order confirmed for chai`;
+    }
+    return confirmOrder()
+}
+let customersOrder = orderTea("Green")
+console.log(customersOrder)
+
+/* 
+3. Write an arrow function named `calculateTotal` that takes two parameters: `price` and `quantity`. The function should return the total cost by multiplying the `price` and `quantity`. 
+Store the result in a variable named `totalCost`.
+ */
+
+const calculateTotal = (price, quantity) => price * quantity;
+let totalCost = calculateTotal(10, 3)
+console.log(totalCost)
+/* 
+4. Write a function named `processTeaOrder` that takes another function, `makeTea`, as a parameter and calls it with the argument `"earl grey"`. 
+Return the result of calling `makeTea`.
+*/
+
+
+
+/* 
+5. Write a function named `createTeaMaker` that returns another function. The returned function should take one parameter, `teaType`, and return a message like `"Making green tea"`. 
+Store the returned function in a variable named `teaMaker` and call it with `"green tea"`.
+*/
+
