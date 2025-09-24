7324de7d39d25397b95e47e181ba2366f40b91b2
Jay Scott <jamie.scott89@live.co.uk>
2025-09-24 13:22:08 +0100

    finished the functions lesson, the challenges are next

---
+++ JavaScript/03_challenges/hands-on.js
@@ -0,0 +1,13 @@
+function stringToNumber(input) {
+  let result = Number(input);
+  if (result === NaN) {
+    return `"Not a number"`;
+  }
+  return result;
+}
+console.log(stringToNumber("foo"));
+function flipBoolean(input) {}
+
+function whatAmI(input) {}
+
+function isItTruthy(input) {}
