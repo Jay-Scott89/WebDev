19038e95e89dcc163a0afa5328c68a1d9f83edb1
Jay-Scott89 <jamie.scott3495@outlook.com>
2025-09-29 21:45:26 +0100

    dynamic list

--- Projects/Dashboard/script.js
+++ Projects/Dashboard/script.js
@@ -6,3 +6,31 @@ function myFunction() {
     x.style.display = "block";
   }
 }
+
+async function loadData() {
+  try {
+    //Get the JSON file
+    const response = await fetch("data.json");
+    if (!response.ok) throw new Error("Network error");
+
+    // Convert into an object
+    const data = await response.json();
+
+    // insert name and location
+    document.getElementById("dynamic-list").innerHTML = data
+      .map(
+        (item) =>
+          `<p>Name: ${item.name}, Lat: ${item.location.lat}, Lng: ${item.location.lng}</p>`
+      )
+      .join("");
+  } catch (error) {
+    // If something is wrong...
+    console.error(error);
+    document.getElementById(
+      "dynamic-list"
+    ).innerHTML = `<p>Failed to load data</p>`;
+  }
+}
+
+// Run functions when page finishes loading
+window.addEventListener("DOMContentLoaded", loadData);
